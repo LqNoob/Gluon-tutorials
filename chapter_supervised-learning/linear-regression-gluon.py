5d5e978e308ef1e61521acef45dfd52dd5bbce96
Kidding <1944189618@qq.com>
2017-12-19 14:10:54 +0800

    Update linear-regression-gluon.py

--- chapter_supervised-learning/linear-regression-gluon.py
+++ chapter_supervised-learning/linear-regression-gluon.py
@@ -1,3 +1,6 @@
+'''
+Linear Regression with Gluon
+'''
 from mxnet import ndarray as nd
 from mxnet import autograd
 from mxnet import gluon
@@ -18,13 +21,34 @@ batch_size = 10
 dataset = gluon.data.ArrayDataset(X, y)
 data_iter = gluon.data.DataLoader(dataset, batch_size, shuffle=True)
 
+# step 3 : Define the model
 net = gluon.nn.Sequential()
 net.add(gluon.nn.Dense(1))
 
+# step 4 : Initialize the model parameters
 net.initialize()
 
+# step 5 : Loss function
 square_loss = gluon.loss.L2Loss()
 
+# step 6 : Optimize function
 trainer = gluon.Trainer(
   net.collect_params(), 'sgd', {'learning_rate': 0.1})
 
+# step 7 : Train
+epochs = 5
+batch_size = 10
+for e in range(epochs):
+    total_loss = 0
+    for data, label in data_iter:
+        with autograd.record():
+            output = net(data)
+            loss = square_loss(output, label)
+        loss.backward()
+        trainer.step(batch_size)
+        total_loss += nd.sum(loss).asscalar()
+    print("Epoch %d, average loss: %f" % (e, total_loss/num_examples))
+    
+dense = net[0]
+dense.weight.data()
+dense.bias.data()
